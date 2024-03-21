03381bce8c99e8a995bea7c604e444496bcc51c2
hans <im.hansking@gmail.com>
2024-03-21 18:04:34 +0800

    🐛 fix: modify .env.example ZEROONE_API_KEY
    and
    
    debug llm.ZeroOne.token.placeholder Error

--- src/locales/default/setting.ts
+++ src/locales/default/setting.ts
@@ -202,6 +202,14 @@ export default {
         title: 'API Key',
       },
     },
+    ZeroOne: {
+      title: '零一万物',
+      token: {
+        desc: '填入来自零一万物的 API Key',
+        placeholder: '零一万物 API Key',
+        title: 'API Key',
+      },
+    },
     Zhipu: {
       title: '智谱',
       token: {
