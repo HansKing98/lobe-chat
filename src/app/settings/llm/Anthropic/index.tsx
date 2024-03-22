6b92a575cf49966d7a2506ab2aaba5927050197b
hans <im.hansking@gmail.com>
2024-03-22 15:49:17 +0800

    🐛 fix: delete never used code

--- src/app/settings/llm/Anthropic/index.tsx
+++ src/app/settings/llm/Anthropic/index.tsx
@@ -1,6 +1,6 @@
 import { Anthropic, Claude } from '@lobehub/icons';
-import { Input, Divider } from 'antd';
-import { useTheme } from 'antd-style';
+import { Divider, Input } from 'antd';
+// import { useTheme } from 'antd-style';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
@@ -9,18 +9,14 @@ import { ModelProvider } from '@/libs/agent-runtime';
 
 import Checker from '../components/Checker';
 import ProviderConfig from '../components/ProviderConfig';
-import {
-  LLMProviderApiTokenKey,
-  LLMProviderBaseUrlKey,
-  LLMProviderConfigKey
-} from '../const';
+import { LLMProviderApiTokenKey, LLMProviderBaseUrlKey, LLMProviderConfigKey } from '../const';
 
 const providerKey = 'anthropic';
 
 const AnthropicProvider = memo(() => {
   const { t } = useTranslation('setting');
 
-  const theme = useTheme();
+  // const theme = useTheme();
 
   return (
     <ProviderConfig
@@ -37,12 +33,7 @@ const AnthropicProvider = memo(() => {
           name: [LLMProviderConfigKey, providerKey, LLMProviderApiTokenKey],
         },
         {
-          children: (
-            <Input
-              allowClear
-              placeholder={t('llm.Anthropic.endpoint.placeholder')}
-            />
-          ),
+          children: <Input allowClear placeholder={t('llm.Anthropic.endpoint.placeholder')} />,
           desc: t('llm.Anthropic.endpoint.desc'),
           label: t('llm.Anthropic.endpoint.title'),
           name: [LLMProviderConfigKey, providerKey, LLMProviderBaseUrlKey],
